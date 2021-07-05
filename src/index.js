fe6cea975fd39722419d70b9886d2f8c4eb8ab8e
acecycle4 <adeleke.bright@aceafrica.net>
2021-07-05 01:07:05 +0100

    Initial COMIT FOR THE AWS AMPLIFY FOR PROJECT

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from "./TodoList"
+import Hello from "./Hello"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <Hello/>
   </React.StrictMode>,
   document.getElementById('root')
 );
